47c301b43922e843f2c4c7f589c2ff563f1df8db
Jakhaar <joao.antonio@more-managedservices.de>
2023-08-09 11:55:58 +0200

    Made the login functional

---
+++ interfaces/main.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append(r'C:\Users\Joao.Antonio\Desktop\Projekte\task-handling-tool\interfaces\screens')
+
+import login_screen as ls
+from tkinter import Tk, Canvas, Entry, Button, PhotoImage
+
+class Main:
+    def __init__(self, parent):
+        
+        pass
+
+
+if __name__ == "__main__":
+    login_app = ls.LoginScreen()
+    login_app.window.mainloop()
--- interfaces/screens/login_screen.py
+++ interfaces/screens/login_screen.py
@@ -1,115 +1,124 @@
 from pathlib import Path
-from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+from tkinter import Tk, Canvas, Entry, Button, PhotoImage
 import _color_palette as cp
+import dashboard as db
 
-OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\Joao.Antonio\Desktop\Projekte\task-handling-tool\data\assets\frame0")
-password = 'Ham'
+class LoginScreen:
+    def __init__(self):
+        self.window = Tk()
+        self.fix_width = 1200
+        self.fix_height = 680
 
-def relative_to_assets(path: str) -> Path:
-    return ASSETS_PATH / Path(path)
+        self.window.geometry(f"{self.fix_width}x{self.fix_height}")
+        self.window.configure(bg=cp.main_color)
 
-def checkLogin():
-    if entry_password.get() == password:
-        print('JA')
-    else: print('ne')
+        self.canvas = Canvas(
+            self.window,
+            bg=cp.main_color,
+            height=self.fix_height,
+            width=self.fix_width,
+            bd=0,
+            highlightthickness=0,
+            relief="ridge"
+        )
 
-window = Tk()
-fix_width = 1200
-fix_height = 680
+        self.canvas.place(x=0, y=0)
+        self.canvas.create_rectangle(
+            195.0,
+            86.0,
+            1005.0,
+            805.0,
+            fill=cp.accent_color_addition,
+            outline=""
+        )
 
-window.geometry(f"{fix_width}x{fix_height}")
-window.configure(bg = cp.main_color)
+        self.entry_image = PhotoImage(
+            file=self.relative_to_assets("entry_1.png"))
+        self.entry_bg = self.canvas.create_image(
+            603.897216796875,
+            339.72332763671875,
+            image=self.entry_image
+        )
 
-canvas = Canvas(
-    window,
-    bg = cp.main_color,
-    height = 680,
-    width = 1200,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
+        self.entry_password = Entry(
+            bd=0,
+            bg=cp.accent_color,
+            fg="#000716",
+            highlightthickness=0
+        )
+        self.entry_password.place(
+            x=377.0,
+            y=290.0,
+            width=453.79443359375,
+            height=97.4466552734375
+        )
 
-canvas.place(x = 0, y = 0)
-canvas.create_rectangle(
-    195.0,
-    86.0,
-    1005.0,
-    805.0,
-    fill=cp.accent_color_addition,
-    outline="")
+        self.canvas.create_text(
+            389.2727355957031,
+            308.814208984375,
+            anchor="nw",
+            text="Password",
+            fill="#6B9080",
+            font=("Montserrat Bold", 14 * -1)
+        )
 
-entry_image = PhotoImage(
-    file=relative_to_assets("entry_1.png"))
-entry_bg = canvas.create_image(
-    603.897216796875,
-    339.72332763671875,
-    image=entry_image
-)
-entry_password = Entry(
-    bd=0,
-    bg=cp.accent_color,
-    fg="#000716",
-    highlightthickness=0
-)
-entry_password.place(
-    x=377.0,
-    y=290.0,
-    width=453.79443359375,
-    height=97.4466552734375
-)
+        self.canvas.create_text(
+            357.0,
+            164.0,
+            anchor="nw",
+            text="Entsperren",
+            fill="#6B9080",
+            font=("Montserrat Bold", 32 * -1)
+        )
 
-canvas.create_text(
-    389.2727355957031,
-    308.814208984375,
-    anchor="nw",
-    text="Password",
-    fill="#6B9080",
-    font=("Montserrat Bold", 14 * -1)
-)
+        self.button_image_1 = PhotoImage(
+            file=self.relative_to_assets("button_1.png"))
+        self.button_1 = Button(
+            image=self.button_image_1,
+            borderwidth=0,
+            highlightthickness=0,
+            command=self.checkLogin,
+            relief="flat"
+        )
+        self.button_1.place(
+            x=538.0,
+            y=484.0,
+            width=123.11282348632812,
+            height=95.91534423828125
+        )
 
-canvas.create_text(
-    357.0,
-    164.0,
-    anchor="nw",
-    text="Entsperren",
-    fill="#6B9080",
-    font=("Montserrat Bold", 32 * -1)
-)
+        self.canvas.create_text(
+            358.0,
+            209.0,
+            anchor="nw",
+            text="um Ihre Aufgaben zu sehen",
+            fill="#A4C3B2",
+            font=("Montserrat SemiBold", 20 * -1)
+        )
 
-button_image_1 = PhotoImage(
-    file=relative_to_assets("button_1.png"))
-button_1 = Button(
-    image=button_image_1,
-    borderwidth=0,
-    highlightthickness=0,
-    command=checkLogin,
-    relief="flat"
-)
-button_1.place(
-    x=538.0,
-    y=484.0,
-    width=123.11282348632812,
-    height=95.91534423828125
-)
+        self.canvas.create_text(
+            389.522216796875,
+            332.7003173828125,
+            anchor="nw",
+            text="123password",
+            fill="#000000",
+            font=("Montserrat SemiBold", 32 * -1)
+        )
+        self.window.resizable(False, False)
+        # self.window.mainloop()
 
-canvas.create_text(
-    358.0,
-    209.0,
-    anchor="nw",
-    text="um Ihre Aufgaben zu sehen",
-    fill="#A4C3B2",
-    font=("Montserrat SemiBold", 20 * -1)
-)
+    def relative_to_assets(self, path: str) -> Path:
+        ASSETS_PATH = Path(r"C:\Users\Joao.Antonio\Desktop\Projekte\task-handling-tool\data\assets\frame0")
+        return ASSETS_PATH / Path(path)
 
-canvas.create_text(
-    389.522216796875,
-    332.7003173828125,
-    anchor="nw",
-    text="123password",
-    fill="#000000",
-    font=("Montserrat SemiBold", 32 * -1)
-)
-window.resizable(False, False)
-window.mainloop()
+    def checkLogin(self):
+        if self.entry_password.get() == 'a':
+            print('JA')
+            self.window.withdraw()
+            self.openDashboard()
+        else:
+            print('ne')
+
+    def openDashboard(self):
+        db.Dashboard(self.window)
+        
