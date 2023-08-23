33d629e20b56024774a4f6f8e2efe1d4337c2da7
Jakhaar <joao.antonio@more-managedservices.de>
2023-08-23 11:46:01 +0200

    Screen Logic besitzt jetzt die einige funktionen

--- data/logic/screen_logic.py
+++ data/logic/screen_logic.py
@@ -1,5 +1,8 @@
+import os
+from pathlib import Path
 from tkinter import messagebox
-import login_screen as ls
+import customtkinter as ctk
+from ...interfaces import login_screen as ls
 
 def logout(window):
     confirm = messagebox.askyesno(
@@ -10,15 +13,21 @@ def logout(window):
         window = ls.LoginScreen()
         window.window.mainloop()
 
-def handle_btn_press(window):
-    # window.forget()
+def relative_to_assets(self, path: str) -> Path:
+    ASSETS_PATH = Path(os.getcwd() + r"/data/assets/main_screen")
+    return ASSETS_PATH / Path(path)
 
-    # match screen:
-    #     case 'dash':
-    #         pass
-    #     case 'dash':
-    #         pass
-    #     case 'dash':
-    #         pass
-    print(window)
-    
+def add_task(self, frame, task):
+    if self.max_task > 0:
+        task_checkbox = ctk.CTkCheckBox(frame, text=task)
+        task_checkbox.pack(fill='x')
+
+def delete_task(self, frame):
+    selected_checkboxes = []
+
+    for widget in frame.winfo_children():
+        if isinstance(widget, ctk.CTkCheckBox) and widget.get() == 1:
+            selected_checkboxes.append(widget)
+
+    for checkbox in selected_checkboxes:
+        checkbox.destroy()    
