b6490d84bfe302bd35de6b277f4ad13fbbede4b3
Jakhaar <joao.antonio@more-managedservices.de>
2023-09-05 14:59:24 +0200

    Update comments and formatting
    - Added comments for clarity
    - Formatted code for consistency

--- interfaces/login_screen.py
+++ interfaces/login_screen.py
@@ -1,3 +1,4 @@
+# Import necessary libraries and modules
 from pathlib import Path
 from tkinter import Tk, Canvas, Entry, Button, PhotoImage
 import _color_palette as cp
@@ -6,47 +7,57 @@ import os
 import sys
 import customtkinter as ctk
 
-# setting path
+# Set the path for custom modules
 sys.path.append(os.getcwd() + r"/data/logic/")
 
+# Import the screen logic module
 import screen_logic as sl
 
-code = 'aaa'
+# Initialize an empty password
+code = ''
 
+# Create the login screen class
 class LoginScreen:
     def __init__(self):
+        # Initialize the login window
         self.window = ctk.CTk()
         self.window.geometry("568x308")
         self.window.title('EXCHANGE')
         self.window.configure(bg=cp.background_color)
 
-        label = ctk.CTkLabel(self.window, text="Passwort eingeben zum entsperren")
-        label.place(x = 90, y = 70)
+        # Create a label for entering the password
+        label = ctk.CTkLabel(self.window, text="Enter the password to unlock")
+        label.place(x=90, y=70)
 
+        # Create an input field for password entry
         eingabe = ctk.CTkEntry(self.window, width=420, height=55)
-        eingabe.place(x = 90, y = 100)
+        eingabe.place(x=90, y=100)
 
+        # Create a login button
         self.login_button = ctk.CTkButton(
             self.window, 
-            text = 'Entsperren',
-            width = 420,
-            height = 55,
-            fg_color = cp.main_color, 
-            text_color = cp.accent_color,
-            hover_color = cp.second_color,
-            command = lambda: self.openMainScreen(eingabe.get()))
-        self.login_button.place(x = 90,y = 170)
+            text='Unlock',
+            width=420,
+            height=55,
+            fg_color=cp.main_color, 
+            text_color=cp.accent_color,
+            hover_color=cp.second_color,
+            command=lambda: self.openMainScreen(eingabe.get()))
+        self.login_button.place(x=90, y=170)
 
+        # Make the window non-resizable
         self.window.resizable(False, False)
    
+    # Function to determine the path relative to assets
     def relative_to_assets(self, path: str) -> Path:
         ASSETS_PATH = Path(os.getcwd() + r"/data/assets/login")
         return ASSETS_PATH / Path(path)
         
-    def openMainScreen(self, login):
-        if login:
-            #TODO: checken ob der Service überhaupt aktiv ist
+    # Function to open the main screen if the password is correct
+    def openMainScreen(self, password):
+        if password == code:
+            # TODO: Check if the service is active
             self.window.destroy()
             
             obj = ms.MainScreen()
-            obj.window.mainloop()        
+            obj.window.mainloop()
--- interfaces/main_screen.py
+++ interfaces/main_screen.py
@@ -1,91 +1,98 @@
+# Import necessary libraries and modules
 import customtkinter as ctk
 import _color_palette as cp
 import os
 import sys
 
-# setting path
+# Set the path for custom modules
 sys.path.append(os.getcwd() + r"/data/logic/")
 
+# Import the screen logic module
 import screen_logic as sl
 
+# Create the main application window
 class MainScreen:
     def __init__(self):
+        # Initialize the main window
         self.window = ctk.CTk()
         self.window.geometry("1200x680")
         self.window.title('EXCHANGE')
         self.window.configure(bg=cp.background_color)
 
+        # Initialize the task list and maximum task count
         self.task_list = []
-
         self.max_task = 13
         
-        # Buttons layout
+        # Create buttons for various actions
         run_button = ctk.CTkButton(
             self.window, 
-            text = 'Task Ausführen',
+            text='Run Task',
             width=180,
             height=32,
-            fg_color = cp.main_color, 
-            text_color = cp.accent_color,
-            hover_color = cp.second_color,
-            command = lambda: print('Run'))
-        run_button.place(x=8,y=100)
+            fg_color=cp.main_color, 
+            text_color=cp.accent_color,
+            hover_color=cp.second_color,
+            command=lambda: print('Run'))
+        run_button.place(x=8, y=100)
 
         add_button = ctk.CTkButton(
             self.window, 
-            text = 'Hizufügen',
+            text='Add Task',
             width=180,
             height=32,
-            fg_color = cp.main_color, 
-            text_color = cp.accent_color,
-            hover_color = cp.second_color,
-            command = lambda: self.add(toggle_frame=toggle_frame))
-        add_button.place(x=8,y=140)
+            fg_color=cp.main_color, 
+            text_color=cp.accent_color,
+            hover_color=cp.second_color,
+            command=lambda: self.add(toggle_frame=toggle_frame))
+        add_button.place(x=8, y=140)
 
         del_button = ctk.CTkButton(
             self.window, 
-            text = 'Löschen',
+            text='Delete Task',
             width=180,
             height=32,
-            fg_color = cp.main_color, 
-            text_color = cp.accent_color,
-            hover_color = cp.second_color,
-            command = lambda: self.delete(toggle_frame))
-        del_button.place(x=8,y=180)
+            fg_color=cp.main_color, 
+            text_color=cp.accent_color,
+            hover_color=cp.second_color,
+            command=lambda: self.delete(toggle_frame=toggle_frame))
+        del_button.place(x=8, y=180)
         
         reset_button = ctk.CTkButton(
             self.window, 
-            text = 'Dienst Neustart',
+            text='Restart Service',
             width=180,
             height=32,
-            fg_color = cp.main_color, 
-            text_color = cp.accent_color,
-            hover_color = cp.second_color,
-            command = lambda: print('Dienst Neustart'))
-        reset_button.place(x=8,y=540)
+            fg_color=cp.main_color, 
+            text_color=cp.accent_color,
+            hover_color=cp.second_color,
+            command=lambda: print('Service Restart'))
+        reset_button.place(x=8, y=540)
 
         term_button = ctk.CTkButton(
             self.window, 
-            text = 'Dienst Beenden',
+            text='Stop Service',
             width=180,
             height=32,
-            fg_color = cp.delete_color, 
-            text_color = '#000',
-            hover_color = cp.delete_color_addition,
-            command = lambda: sl.logout())
-        term_button.place(x=8,y=590)
+            fg_color=cp.delete_color, 
+            text_color='#000',
+            hover_color=cp.delete_color_addition,
+            command=lambda: sl.logout())
+        term_button.place(x=8, y=590)
 
-        # Toggle layout
+        # Create a frame for task toggles
         toggle_frame = ctk.CTkFrame(self.window)
         toggle_frame.place(x=8, y=220)
 
+        # Make the window non-resizable and start the main loop
         self.window.resizable(False, False)
         self.window.mainloop()
 
+    # Function to add a new task
     def add(self, toggle_frame):
         if self.max_task > 0:
-            sl.add_task(frame = toggle_frame, task=f'Transfer_Status_')
+            sl.add_task(frame=toggle_frame, task=f'Transfer_Status_')
             self.max_task -= 1
         
+    # Function to delete a task
     def delete(self, toggle_frame):
         self.max_task += sl.delete_task(toggle_frame)
